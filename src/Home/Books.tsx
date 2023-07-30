f41c349206ade3cf2b4180cceb030fc10d198fea
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-30 16:34:19 +0600

    set up

--- src/Home/Books.tsx
+++ src/Home/Books.tsx
@@ -1,6 +1,6 @@
 import { useGetBooksQuery } from "../redux/api/bookapi";
 import { IBook } from "../type/booktype";
-
+import bookImage from "../images/Fiction-Book-Covers.webp"
 const Books = () => {
     const { data, isLoading, error } = useGetBooksQuery(undefined);
 
@@ -13,14 +13,29 @@ const Books = () => {
     }
 
 
-    return (
-        <div>
+  return (
+    <div className="mt-10">
+      <h1 className="text-center text-3xl my-6 ">Books Collection</h1>
+        <div className="grid grid-cols-3 gap-2">
             {data.slice().reverse().slice(0, 10).map((book: IBook ) => (
-                <div key={book._id}>
-                    <p>{book.Author}</p>
-                </div>
+               
+                    
+            
+                     <div  key={book._id} className="card card-compact w-60 bg-base-100 shadow-xl mx-auto my-12">
+  <figure style={{height:'300px'}}><img  src={bookImage} alt="Shoes" /></figure>
+  <div className="card-body">
+                  <h2 className="card-title">{book.Title}</h2>
+   
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Details</button>
+    </div>
+  </div>
+</div>
+                  
+               
             ))}
         </div>
+      </div>
     );
 };
 

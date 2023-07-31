eec642f0a55ec2c5f13a354fe636022dac50b83e
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-07-31 11:45:12 +0600

    vercel backend link add

--- src/redux/api/bookapi.ts
+++ src/redux/api/bookapi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const bookapi = createApi({
     reducerPath: 'bookapi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://bookshop-backend-git-main-mdnadiruzzamannibras.vercel.app" }),
     endpoints: (builder) => ({
         getBooks: builder.query({
             query: () => "/books"

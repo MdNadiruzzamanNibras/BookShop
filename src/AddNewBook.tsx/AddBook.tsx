d0c8e77bc900bcdd0550d85e171fd0d41d52e11c
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-28 20:40:48 +0600

    add book from

--- src/AddNewBook.tsx/AddBook.tsx
+++ src/AddNewBook.tsx/AddBook.tsx
@@ -1,43 +1,65 @@
 
+
 const AddBook = () => {
-    return (
-        <div>
-            <div className="flex justify-center items-center h-screen">
-                <div className="flex justify-center items-center">
-                    <form className="w-96 bg-base-100">
-                    <div>
-                        <label >Title</label>
-                         <input type="text" name="title"  className="input input-bordered w-full max-w-xs my-4" />
-                    </div>
-                    <div>
-                        <label >Author</label>
-                         <input type="text" name="author"  className="input input-bordered w-full max-w-xs my-4" />
-                    </div>
-                    <div>
-      <label>Genre</label>
-      <select  name="genre" id="mySelect">
-        <option value="Love">Option 1</option>
-        <option value="any">Option 2</option>
-        <option value="option3">Option 3</option>
-        <option value="option4">Option 4</option>
-        <option value="option5">Option 5</option>
-      </select>
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log("add book");
+    }
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-4">Add a New Book</h2>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="border p-2 w-full rounded-md"
+              placeholder="Enter the book title"
+            />
+          </div>
+          <div>
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              name="author"
+              id="author"
+              className="border p-2 w-full rounded-md"
+              placeholder="Enter the author's name"
+            />
+          </div>
+          <div>
+            <label htmlFor="genre">Genre</label>
+            <select name="genre" id="genre" className="border p-2 w-full rounded-md">
+              <option value="Love">Love</option>
+              <option value="Adventure">Adventure</option>
+              <option value="Mystery">Mystery</option>
+              <option value="Science Fiction">Science Fiction</option>
+              <option value="Fantasy">Fantasy</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="year">Year</label>
+            <select name="year" id="year" className="border p-2 w-full rounded-md">
+              <option value="2018">2018</option>
+              <option value="2012">2012</option>
+              <option value="2023">2023</option>
+              <option value="2021">2021</option>
+              <option value="2019">2019</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+          >
+            Add Book
+          </button>
+        </form>
+      </div>
     </div>
-                    <div>
-      <label>Year</label>
-      <select  name="pabulication" id="mySelect">
-        <option value="2018">2018</option>
-        <option value="223">2012</option>
-        <option value="1023"></option>
-        
-      </select>
-    </div>
-                   
-                </form>
-                </div>
-         </div>
-        </div>
-    );
+  );
 };
 
 export default AddBook;

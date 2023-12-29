eaa34ebfc508281cd75c92813fc466f696243095
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-29 14:38:52 +0600

    some change add book

--- src/AddNewBook.tsx/AddBook.tsx
+++ src/AddNewBook.tsx/AddBook.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useAppSelector } from "../redux/hooks";
 import { useBookAddMutation } from "../redux/api/bookapi";
 
 
 const AddBook = () => {
     const [bookAdd ] = useBookAddMutation()
-    const [title, setTitle] = useState<string | undefined>(undefined); 
-    const [author, setAuthor] = useState<string | undefined>(undefined);
-    const [genre, setGenre] = useState<string | undefined>(undefined);
-    const [year, setYear] = useState<string | undefined>(undefined);
+    const [title, setTitle] = useState<string | undefined>(""); 
+    const [author, setAuthor] = useState<string | undefined>("");
+    const [genre, setGenre] = useState<string | undefined>("");
+    const [year, setYear] = useState<string | undefined>("");
    const { user, } = useAppSelector(state => state.user)
  
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     
        e.preventDefault();
     const options = {
@@ -27,7 +27,23 @@ const AddBook = () => {
     console.log(bookAdd);
         
         
-     }
+  }
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  setTitle(e.target.value);
+};
+
+const handleAuthorChange = (e: ChangeEvent<HTMLInputElement>) => {
+  setAuthor(e.target.value);
+};
+
+const handleGenreChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  setGenre(e.target.value);
+};
+
+const handleYearChange = (e: ChangeEvent<HTMLSelectElement>) => {
+  setYear(e.target.value);
+};
+
     
   return (
     <form onSubmit={handleSubmit} className="pb-5 px-8 grid grid-cols-12 gap-4">
@@ -39,10 +55,11 @@ const AddBook = () => {
                     <label className="label">
                         <span className="label-text">Book Name</span> 
                     </label> 
-                    <input
+              <input 
+                required
                     type="text" 
                     placeholder="Book name"  
-                    className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black"  onChange={(e) => setTitle(e.target.value)}
+                    className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black"  onChange={handleTitleChange}
                     />
                 </div>
             </div>  
@@ -51,7 +68,7 @@ const AddBook = () => {
                     <label className="label">
                         <span className="label-text">Book Author</span> 
                     </label> 
-                    <input type="text" placeholder="Book author"  className="input input-bordered w-full p-2 focus:outline-none rounded my-2 text-black" onChange={(e) => setAuthor(e.target.value)}/>
+                    <input type="text" required placeholder="Book author"  className="input input-bordered w-full p-2 focus:outline-none rounded my-2 text-black" onChange={handleAuthorChange}/>
                 </div>
             </div>
             <div className="col-span-12">
@@ -59,7 +76,7 @@ const AddBook = () => {
                     <label className="label">
                         <span className="label-text">Genre</span> 
               </label> 
-              <select name="genre" id="genre" className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black" onChange={(e) => setGenre(e.target.value)}>
+              <select name="genre" required id="genre" className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black" onChange={handleGenreChange} >
                 <option >Select Genre</option>
               <option value="Love">Love</option>
               <option value="Adventure">Adventure</option>
@@ -75,7 +92,7 @@ const AddBook = () => {
                     <label className="label">
                         <span className="label-text">Publication Year</span> 
               </label>
-              <select name="year" className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black" onChange={(e) => setYear(e.target.value)}>
+              <select name="year" className="input input-bordered focus:outline-none w-full p-2 rounded my-2 text-black" onChange={handleYearChange}>
               <option >Select Year</option>
               <option value="2018">2018</option>
               <option value="2012">2012</option>

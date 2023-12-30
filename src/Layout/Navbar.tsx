f0e2f2114d7a33cb5eeb87ecd76fabe4c09aa695
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-12-30 22:03:24 +0600

    navbar li text size and add some route

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -30,10 +30,19 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <Link to='/'>
+                <li className="mx-4">Home</li>
+                  
+                </Link> 
         <Link to='/allBook'>
                 <li className="mx-4">All Book</li>
-                  <li className="mx-4">{ books.length}</li>
-              </Link> 
+                  
+                </Link> 
+        <Link to='/AddBook'>
+                <li className="mx-4">Add Book</li>
+                  
+                </Link> 
+                <li className="mx-4">{ books.length}</li>
               {!user.email && <Link to='/login'>
                 <li>login</li>
               </Link>}
@@ -42,14 +51,25 @@ const Navbar = () => {
               }
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">Bookshop</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-              <Link to='/allBook'>
+    <ul className="menu text-xl font-bold menu-horizontal px-1">
+              <Link to='/'>
+                <li className="mx-4">Home</li>
+                  
+                </Link> 
+        <Link to='/allBook'>
                 <li className="mx-4">All Book</li>
+                  
               </Link> 
               <li className="mx-4">wish{ books.length}</li>
+        <Link to='/AddBook'>
+                <li className="mx-4">Add Book</li>
+                  
+                </Link> 
+               
+              
               {!user.email && <Link to='/login'>
                 <li>login</li>
               </Link>}
